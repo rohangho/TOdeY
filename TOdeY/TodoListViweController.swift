e408d1e5bd5e901711eb99f8319d289b8c7aebc4
Rohan Ghosh <rohangho@live.com>
2020-07-26 02:19:54 +0530

    Added tick on Select

---
+++ TOdeY/TodoListViweController.swift
@@ -0,0 +1,46 @@
+//
+//  ViewController.swift
+//  TOdeY
+//
+//  Created by Rohan Ghosh on 26/07/20.
+//  Copyright © 2020 Rohan Ghosh. All rights reserved.
+//
+
+import UIKit
+
+class TodoListViweController: UITableViewController {
+    
+    let itemArray = ["Todo1","Todo2","Todo3","Todo4"]
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+       
+        // Do any additional setup after loading the view.
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return itemArray.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "ToDoListCell", for: indexPath)
+        cell.textLabel?.text = itemArray[indexPath.row]
+        return cell
+    }
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        print(itemArray[indexPath.row])
+        if(tableView.cellForRow(at: indexPath)?.accessoryType == .checkmark)
+        {
+            tableView.cellForRow(at: indexPath)?.accessoryType = .none
+        }
+        else
+        {
+            tableView.cellForRow(at: indexPath)?.accessoryType = .checkmark
+        }
+        tableView.deselectRow(at: indexPath, animated: true)
+    }
+
+
+}
+
