ffc41421c1274c6c9902ba8b015f7919af0078cf
Rohan Ghosh <rohangho@live.com>
2020-08-10 17:58:28 +0530

    Added Category controller

---
+++ TOdeY/Controllers/CategoryViewControllerTableViewController.swift
@@ -0,0 +1,35 @@
+//
+//  CategoryViewControllerTableViewController.swift
+//  TOdeY
+//
+//  Created by Rohan Ghosh on 10/08/20.
+//  Copyright © 2020 Rohan Ghosh. All rights reserved.
+//
+
+import UIKit
+
+class CategoryViewControllerTableViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+
+    // MARK: - Table view data source
+
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        // #warning Incomplete implementation, return the number of sections
+        return 0
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // #warning Incomplete implementation, return the number of rows
+        return 0
+    }
+
+    
+    @IBAction func clickAddCate(_ sender: UIBarButtonItem) {
+    }
+    
+}
